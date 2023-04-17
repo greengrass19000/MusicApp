0933948ef989f08d94994287994f9bfe01c44215
Salicaceae <tranthuyduonghy2003@gmail.com>
2023-04-17 21:31:35 +0700

    add playlist layout

--- app/src/main/java/com/example/musicapp/Activity/PlaylistActivity.java
+++ app/src/main/java/com/example/musicapp/Activity/PlaylistActivity.java
@@ -1,6 +1,9 @@
 package com.example.musicapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -9,8 +12,15 @@ import android.widget.Toast;
 
 import com.example.musicapp.Model.Introduction;
 import com.example.musicapp.R;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class PlaylistActivity extends Activity {
+public class PlaylistActivity extends AppCompatActivity {
+    CoordinatorLayout coordinatorLayout;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    Toolbar toolbar;
+    RecyclerView recyclerViewPlaylist;
+    FloatingActionButton floatingActionButton;
     Introduction introduction;
 
     @Override
@@ -18,6 +28,15 @@ public class PlaylistActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist);
         DataIntent();
+        init();
+    }
+
+    private void init() {
+        coordinatorLayout = findViewById(R.id.coordinatorlayoutplaylist);
+        collapsingToolbarLayout = findViewById(R.id.collapsingtoolbar);
+        toolbar = findViewById(R.id.toolbarplaylist);
+        recyclerViewPlaylist = findViewById(R.id.recyclerviewplaylist);
+        floatingActionButton = findViewById(R.id.floatingactionbutton);
     }
 
     private void DataIntent() {
