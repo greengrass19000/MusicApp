7c6cefe29815218e2d48ed69184dc695264b16d5
Salicaceae <tranthuyduonghy2003@gmail.com>
2023-04-21 22:53:44 +0700

    add favourite song adapter

---
+++ app/src/main/java/com/example/musicapp/Adapter/FavouriteSongAdapter.java
@@ -0,0 +1,60 @@
+package com.example.musicapp.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.musicapp.Model.Song;
+import com.example.musicapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class FavouriteSongAdapter extends RecyclerView.Adapter<FavouriteSongAdapter.ViewHolder> {
+    Context context;
+    ArrayList<Song> songList;
+
+    public FavouriteSongAdapter(Context context, ArrayList<Song> songList) {
+        this.context = context;
+        this.songList = songList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.row_favouritesong, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Song song = songList.get(position);
+        holder.txtSinger.setText(song.getSinger());
+        holder.txtName.setText(song.getName());
+        Picasso.get().load(song.getImg()).into(holder.img);
+    }
+
+    @Override
+    public int getItemCount() {
+        return songList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txtName, txtSinger;
+        ImageView img, imgLikerate;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            txtName = itemView.findViewById(R.id.textviewfavouritesongname);
+            txtSinger = itemView.findViewById(R.id.textviewfavouritesongsinger);
+            img = itemView.findViewById(R.id.imageviewfavouritesong);
+            imgLikerate = itemView.findViewById(R.id.imageviewlikerate);
+        }
+    }
+}
--- app/src/main/java/com/example/musicapp/Fragment/Fragment_FavouriteSong.java
+++ app/src/main/java/com/example/musicapp/Fragment/Fragment_FavouriteSong.java
@@ -5,11 +5,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.musicapp.Adapter.FavouriteSongAdapter;
 import com.example.musicapp.Model.Song;
 import com.example.musicapp.R;
 import com.example.musicapp.Services.APIService;
@@ -24,11 +28,14 @@ import retrofit2.Response;
 
 public class Fragment_FavouriteSong extends Fragment {
     View view;
+    RecyclerView recyclerView;
+    FavouriteSongAdapter favouriteSongAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_favouritesong, container, false);
+        recyclerView = view.findViewById(R.id.recyclerviewfavouritesong);
         GetData();
         return view;
     }
@@ -40,7 +47,11 @@ public class Fragment_FavouriteSong extends Fragment {
             @Override
             public void onResponse(Call<List<Song>> call, Response<List<Song>> response) {
                 ArrayList<Song> songList = (ArrayList<Song>) response.body();
-
+                favouriteSongAdapter = new FavouriteSongAdapter(getActivity(), songList);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+                linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                recyclerView.setLayoutManager(linearLayoutManager);
+                recyclerView.setAdapter(favouriteSongAdapter);
             }
 
             @Override
